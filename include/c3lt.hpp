79d9a50acb384982ed84958879248a55b0b878ab
Cyclic3 <cyclic3.git@gmail.com>
2022-06-26 01:27:20 +0100

    various tweaks to make it easier to use

--- include/c3lt.hpp
+++ include/c3lt.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <concepts>
+#include <type_traits>
+
 namespace c3lt {
   /// Indicates a normal pointer
   template<typename T>
@@ -8,15 +11,19 @@ namespace c3lt {
     T* p = nullptr;
 
   public:
-    inline operator bool() const { return p; }
-    inline operator T*() const { return p; }
-    inline T& operator*() const { return *p; }
-    inline T* operator->() const { return p; }
+    T* get() const { return p; }
+
+  public:
+//    constexpr operator ptr<std::remove_const_t<T>>() const { return p; }
+    template<typename U>
+    constexpr operator ptr<U>() const { return ptr<U>{static_cast<U*>(this->get())}; }
+    constexpr operator bool() const { return p; }
+    constexpr operator T*() const { return p; }
+    constexpr T& operator*() const { return *p; }
+    constexpr T* operator->() const { return p; }
 
   public:
     inline ptr() = default;
-    inline ptr(ptr const&) = default;
-    inline ptr(ptr&&) = default;
     inline ptr(T* p_) : p{p_} {}
   };
 
@@ -24,7 +31,23 @@ namespace c3lt {
   template<typename T>
   class managed : public ptr<T> {
   public:
+    template<typename U>
+    constexpr operator managed<U>() const { return managed<U>{static_cast<U*>(this->get())}; }
+
+  public:
     inline managed() = default;
     inline explicit managed(T* p) : ptr<T>{p} {}
   };
+
+  namespace {
+    struct foo {};
+    struct bar : foo {};
+    [[maybe_unused]]
+    consteval void test() {
+      managed<bar> x;
+      managed<foo> y = x;
+      managed<foo> z = y;
+      z = y;
+    }
+  }
 }
